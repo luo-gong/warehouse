c5bc2d06dc3f1d517dc00696a00d2e7c3eee7bdb
luogong <luogong@qq.com>
2020-05-21 11:17:04 +0800

    仓库管理系统

---
+++ whm-demo/src/main/java/lg/cn/WhmDemoApplication.java
@@ -0,0 +1,13 @@
+package lg.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhmDemoApplication.class, args);
+    }
+
+}
